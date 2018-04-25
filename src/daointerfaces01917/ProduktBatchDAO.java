0e8b4724b988111ec83156778addf2848afbda66
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-04-25 12:31:18 +0200

    Finished ProduktBatchDAO

--- src/daointerfaces01917/ProduktBatchDAO.java
+++ src/daointerfaces01917/ProduktBatchDAO.java
@@ -6,8 +6,8 @@ import java.util.List;
 import dto01917.ProduktBatchDTO;
 
 public interface ProduktBatchDAO {
-	ProduktBatchDTO getProduktBatch(int pbId) throws DALException;
-	List<ProduktBatchDTO> getProduktBatchList() throws DALException;
+	ProduktBatchDTO getProduktBatch(int pbId) throws DALException, SQLException;
+	List<ProduktBatchDTO> getProduktBatchList() throws DALException, SQLException;
 	void createProduktBatch(ProduktBatchDTO produktbatch) throws DALException, SQLException;
-	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException;
+	void updateProduktBatch(ProduktBatchDTO produktbatch) throws DALException, SQLException;
 }
